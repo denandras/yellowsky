27a986a8b7c7be474cd30e2e551606f9ae3b47ce
denandras <d.enandras@gmail.com>
2026-05-22 20:58:59 +0200

    fix: adjust hero focal point to 36%

--- components/home-page-client.tsx
+++ components/home-page-client.tsx
@@ -204,7 +204,7 @@ export default function HomePageClient({ initialLanguage, communityPosts = [] }:
                 <Image
                   alt="Yellowsky German Street sketch - yellow architectural illustration"
                   className="object-cover"
-                  style={{ objectPosition: 'center 39%' }}
+                  style={{ objectPosition: 'center 36%' }}
                   src="/hero.jpg"
                   fill
                   priority
